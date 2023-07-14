bdf0e82013dd65834f41c690cd2c7bb1bbd9144d
davromano <davidesmail00@gmail.com>
2023-07-15 00:52:34 +0200

    image test

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -12,7 +12,7 @@ const ContactsPage = () => {
     <Layout pageTitle="ContactsPage">
       <div className="flex items-center space-x-60" style={{ height: 'calc(100vh - 4rem)' }}>
         <StaticImage
-          src="../images/me.JPG"
+          src="../images/profile_photo.jpg"
           alt="Davide Romano profile photo"
           placeholder="blurred"
           width={400}
